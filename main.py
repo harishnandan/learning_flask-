73c676d069cb18c19b91e581330fe1bf151962c7
harishnandan <harishshivam@gmail.com>
2017-04-07 02:36:27 +0530

    Added template

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def host():
+    return "Welcome to flask"
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html",name=name)
+
+
+if __name__ == '__main__':
+    app.run()
